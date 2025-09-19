dde662d6e3d50e6a12634395490ba67ff6eeb6b6
Hugo Trung <trunghieuk09645@gmail.com>
2025-09-19 22:35:23 +0700

    Add files via upload

---
+++ Contact/main.js
@@ -0,0 +1,24 @@
+const navs = document.querySelectorAll(".nav");
+
+navs.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    navs.forEach((n) => n.classList.remove("active"));
+
+    nav.classList.add("active");
+  });
+});
+
+function openEmail() {
+  const mailtoLink =
+    "mailto:contact@nihongoconnect.org?subject=Xin%20chào&body=Xin%20chào,%20tôi%20quan%20tâm%20đến%20dịch%20vụ...";
+  const gmailWebLink =
+    "https://mail.google.com/mail/?view=cm&fs=1&to=contact@nihongoconnect.org&su=Xin%20chào&body=Xin%20chào,%20tôi%20quan%20tâm%20đến%20dịch%20vụ...";
+
+  if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+    // Nếu là điện thoại → dùng mailto (mở app Gmail)
+    window.location.href = mailtoLink;
+  } else {
+    // Nếu là PC → mở Gmail web
+    window.open(gmailWebLink, "_blank");
+  }
+}
